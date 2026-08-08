e076ffd87a2ad46e0b46e7a0add85a081060b4c2
Kelvin-star07 <kelvinkodaddiaz@gmail.com>
2026-08-08 19:27:23 -0400

    agregando controlador de center IA

--- Elysia backend/Elysia.Presentation.WebApi/Controllers/v1/CenterIAController.cs
+++ Elysia backend/Elysia.Presentation.WebApi/Controllers/v1/CenterIAController.cs
@@ -1,12 +1,106 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Elysia.Core.Application.Dtos.CenterIA;
+using Elysia.Core.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Elysia.Presentation.WebApi.Controllers.v1
 {
-    public class CenterIAController : Controller
+    [ApiVersion("1.0")]
+    [Authorize(Roles = "Propietario")]
+    public class CenterIAController : BaseApiController
     {
-        public IActionResult Index()
+        private readonly ICenterIAService centerIAService;
+
+
+
+        public CenterIAController(ICenterIAService centerIAService)
         {
-            return View();
+            this.centerIAService = centerIAService;
         }
+
+
+
+
+        [HttpGet("get-analisis-restaurante")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAnalisisRestauranteAsync()
+        {
+            try
+            {
+
+                var user_id = User.FindFirst("UId")!.Value;
+                var data = await centerIAService.AnalizarRestauranteAsync(user_id);
+                return Ok(data);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+        }
+
+
+
+
+
+        [HttpPost("consultar-asistente")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ConsultarAsistenteAsync(SolicitudChatIaDto dto)
+        {
+            try
+            {
+
+                var user_id = User.FindFirst("UId")!.Value;
+                var data = await centerIAService.ConsultarAsistenteAsync(user_id, dto);
+                return Ok(data);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+        }
+
+
+
+
+
+        [HttpGet("get-optimizacion-menu")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> OptimizarMenuAsync()
+        {
+            try
+            {
+
+                var user_id = User.FindFirst("UId")!.Value;
+                var data = await centerIAService.OptimizarMenuAsync(user_id);
+                return Ok(data);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+        }
+
+
+
+
     }
 }
